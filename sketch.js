5dd626c95aa41e9d1c88bfdd8e6c4ca9d1e49956
sobral152006 <sobralgabriel538@gmail.com>
2024-06-21 11:57:44 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let cor
+let posicaoHorital; // x
+let posicaoVertical; // y
+
+function setup() {
+createCanvas(400,400);
+background(color(100,0,0));
+cor = color (random(0,255), random(0,225), random(0,100));
+posicaoHorizontal = 200;
+posicaoVertical = 200;
+}            
+
+ function draw() {
+fill(cor);
+circle(posicaoHorizontal,posicaoVertical,50);
+ 
+
+if(mouseX < posicaoHorizontal){
+ posicaoHorizontal = posicaoHorizontal-1;
+}
+ 
+if(mouseX > posicaoHorizontal){ 
+posicaoHorizontal = posicaoHorizontal+1;
+}  
+ 
+if(mouseY < posicaoVertical){
+posicaoVertical--;  
+}
+ 
+if(mouseY > posicaoVertical){
+posicaoVertical++;
+}
+if(mouseIsPressed){
+cor = color(random(0,255),random(0,255),random(0,255));
+}
+ }
